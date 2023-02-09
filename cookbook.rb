c51506ee40e0f603d97eb3ef68910681bfd4eb2f
Emerson.Pretto1 <emerson.pretto@gmail.com>
2023-02-09 20:45:39 -0300

    Criando array

--- cookbook.rb
+++ cookbook.rb
@@ -1,6 +1,10 @@
 puts " Bem vindo ao cookbook sua rede social de receita"
 
+receitas = []
+
 puts "Digite o nome da receita: "
 nome = gets.chomp()
+
+receitas << nome
 
 puts "Receita #{nome} cadastrada com sucesso!"
